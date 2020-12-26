379dbe75fa718239be2465ebef1ff2c84a28840d
Marko Rezić <markorezic1999@gmail.com>
2020-12-26 03:42:05 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -4,12 +4,8 @@ import 'bootstrap/dist/css/bootstrap.css';
 import "./css/blog.css";
 import Navbar from "./components/Navbar";
 import { AuthorityProvider } from './components/AuthorityContext';
-import Axios from 'axios';
 
 function App() {
-
-  Axios.defaults.headers.common["Access-Control-Allow-Origin"] = "https://rt-foto-editor.netlify.app";
-
   return (
     <AuthorityProvider>
       <div className="App">
