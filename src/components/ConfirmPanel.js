198f683999641a91ed22f7eb99edb06797242c0c
Marko Rezić <markorezic1999@gmail.com>
2021-01-16 03:31:42 +0100

    email verification

--- src/components/ConfirmPanel.js
+++ src/components/ConfirmPanel.js
@@ -1,13 +1,31 @@
-
+import Axios from 'axios';
 
 const ConfirmPanel = () => {
+    
+    const [{ loginStatus, authority }, setAuthority, userList, setUserList, currentUser, setCurrentUser] = useContext(AuthorityContext);
+    Axios.defaults.withCredentials = true;
+    let url = 'https://rt-foto-editor.herokuapp.com';
+    let id = currentUser.id;
+    let email = currentUser.email;
+    let sent = false;
+
+    function resendEmail(){
+        Axios.post(url + '/confirmation/send', {
+            id: id,
+            email: email
+        }).then(() => {
+            sent = true;
+        });
+    }
+
     return (
         <div className="blog-header no-border no-margin">
             <div className="container btrans no-padding">
                 <h4 className="text-center text-white"><strong>Please confirm your email</strong></h4>
                 <h6 className="text-center">
                     <strong>Didn't recieve an email?</strong>
-                    <button className="resendButton">Resend email</button>
+                    <button className="resendButton" onClick={resendEmail}>Resend email</button>
+                    {sent ? 'Check your email' : null}
                 </h6>
 
             </div>
