d7d0ada7543d79c7fade3413821be6e8d22ba15b
Marko Rezić <markorezic1999@gmail.com>
2021-01-30 19:09:01 +0100

    adding views

--- src/components/Users.js
+++ src/components/Users.js
@@ -2,42 +2,32 @@ import { useState, useContext, useEffect } from 'react';
 import Axios from 'axios';
 import { AuthorityContext } from './AuthorityContext';
 
-const Users = () =>{
+const Users = () => {
     const [{ loginStatus, authority }, setAuthority, userList, setUserList, currentUser, setCurrentUser] = useContext(AuthorityContext);
     Axios.defaults.withCredentials = true;
     let url = 'https://rt-foto-editor.herokuapp.com';
+    const [users, SetUsers] = useState([]);
 
     useEffect(() => {
         Axios.get(url + '/users').then((response) => {
             setUserList([...response.data]);
-            console.log(userList);
+            SetUsers([...response.data]);
         });
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
-    function showUsers(){
-        Axios.get(url + '/users').then((response) => {
-            setUserList([...response.data]);
-            console.log(userList);
-        });
-    }
-
     return (
         <div>
             {
-                showUsers
-            }
-            { userList.length 
-            ? userList.map(user => {
-                <div key={user.id}>
-                    <p>Username: {user.displayname}</p>
-                    <p>Email: {user.email}</p>
-                    <p>ID: {user.id}</p>
-                    <button>Remove</button>
-                    <button>Update</button>
-                </div>
-            })
-            : null
+                users.map(user => {
+                    <div key={user.id}>
+                        <p>Username: {user.displayname}</p>
+                        <p>Email: {user.email}</p>
+                        <p>ID: {user.id}</p>
+                        <button>Remove</button>
+                        <button>Update</button>
+                    </div>
+                })
             }
         </div>
     )
