d3d1f74f1d2c45ddc0efee0ba360655121c6ce2c
Marko Rezić <markorezic1999@gmail.com>
2021-01-30 19:27:28 +0100

    adding views

--- src/components/Users.js
+++ src/components/Users.js
@@ -2,25 +2,16 @@ import { useState, useContext, useEffect } from 'react';
 import Axios from 'axios';
 import { AuthorityContext } from './AuthorityContext';
 
-const Users = () => {
+const Users = (props) => {
     const [{ loginStatus, authority }, setAuthority, userList, setUserList, currentUser, setCurrentUser] = useContext(AuthorityContext);
     Axios.defaults.withCredentials = true;
     let url = 'https://rt-foto-editor.herokuapp.com';
-    const [users, SetUsers] = useState([]);
-
-    useEffect(() => {
-        Axios.get(url + '/users').then((response) => {
-            setUserList([...response.data]);
-            console.log(userList);
-        });
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+    const [users, SetUsers] = useState([...props.users]);
 
     return (
         <div>
             {
                 users.map(user => {
-                    SetUsers([...userList]);
                     <div key={user.id}>
                         <p>Username: {user.displayname}</p>
                         <p>Email: {user.email}</p>
