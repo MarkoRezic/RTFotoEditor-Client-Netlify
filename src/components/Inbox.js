d376773f464323220c966e5d4db37ab64f3be85f
Marko Rezić <markorezic1999@gmail.com>
2021-02-12 17:08:40 +0100

    adding chat

--- src/components/Inbox.js
+++ src/components/Inbox.js
@@ -11,8 +11,9 @@ const Inbox = () => {
     let url = 'https://rt-foto-editor.herokuapp.com';
     //let url = 'http://localhost:3001';
 
-    const [messages, setMessages] = useState([]);
+    const [messagesRecieved, setMessagesRecieved] = useState([]);
     const [messagesSent, setMessagesSent] = useState([]);
+    const [messages, setMessages] = useState([]);
     const [username, setUsername] = useState('');
     const [text, setText] = useState('');
     const [sentText, setSentText] = useState('');
@@ -24,7 +25,11 @@ const Inbox = () => {
         }).then(() => {
             updateMessages();
         }
-        );
+        ).then(() => {
+            setMessages([...mergeChunks(makeChunks(messagesRecieved, "sender_id"), makeChunks(messagesSent, "reciever_id"), "sender_id", "reciever_id")].sort(function (a, b) {
+                return b[0]["id"] - a[0]["id"];
+            }))
+        });
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [currentUser]);
 
@@ -42,9 +47,9 @@ const Inbox = () => {
         return '';
     }
 
-    function updateMessages(){
+    function updateMessages() {
         Axios.get(url + '/messages/' + currentUser.id).then((response) => {
-            setMessages([...response.data].reverse());
+            setMessagesRecieved([...response.data].reverse());
         });
         Axios.get(url + '/messages-sent/' + currentUser.id).then((response) => {
             setMessagesSent([...response.data].reverse());
@@ -60,7 +65,7 @@ const Inbox = () => {
             setUsernameError('');
             Axios.post(url + '/send-message', { sender_id: currentUser.id, reciever_id: findID(username), text: text }).then((response) => {
                 console.log(response);
-            }).then(()=>{
+            }).then(() => {
                 updateMessages();
             });
         }
@@ -69,31 +74,78 @@ const Inbox = () => {
         }
     }
 
-    function replyFocus(usernameReply){
+    function replyFocus(usernameReply) {
         document.getElementById('newMessageText').focus();
         setUsername(usernameReply);
         document.getElementById('newMessageUsername').value = usernameReply;
     }
 
-    function deleteMessage(message_id){
+    function deleteMessage(message_id) {
         Axios.delete(url + '/remove-message', { data: { message_id: message_id } }).then((response) => {
             console.log(response);
             updateMessages();
         });
     }
 
-    function deleteAllRecieved(reciever_id){
+    function deleteAllRecieved(reciever_id) {
         Axios.delete(url + '/remove-messages-recieved', { data: { reciever_id: reciever_id } }).then((response) => {
             console.log(response);
             updateMessages();
         });
     }
 
-    function deleteAllSent(sender_id){
+    function deleteAllSent(sender_id) {
         Axios.delete(url + '/remove-messages-sent', { data: { sender_id: sender_id } }).then((response) => {
             console.log(response);
             updateMessages();
         });
+    }
+
+    function makeChunks(array, property) {
+        let chunked_array = [];
+        let copied_array = [...array];
+        let current_element;
+        while (copied_array.length > 0) {
+            current_element = copied_array[0];
+            var new_chunk = [];
+            for (var i = 0; i < copied_array.length; i++) {
+                if (copied_array[i][property] === current_element[property]) {
+                    new_chunk.push(copied_array[i]);
+                    copied_array.splice(i, 1);
+                    i--;
+                }
+            }
+            chunked_array.push(new_chunk);
+        }
+        return chunked_array;
+    }
+
+    function mergeChunks(array1, array2, property1, property2) { //merge where array1's elements property1 === array2's elements property2
+        let merged_array = [...array1];
+        let copied_array = [...array2];
+        let original_length = merged_array.length;
+        let found;
+        while (copied_array.length > 0) {
+            found = false;
+            for (var i = 0; i < original_length; i++) {
+                if (merged_array[i][0][property1] === copied_array[0][0][property2]) {
+                    while (copied_array[0].length > 0) {
+                        merged_array[i].push(copied_array[0][0]);
+                        copied_array[0].splice(0, 1);
+                    }
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) merged_array.push(copied_array[0]);
+            copied_array.splice(0, 1);
+        }
+        for (var i = 0; i < merged_array.length; i++) {
+            merged_array[i] = [...merged_array[i].sort(function (a, b) {
+                return b["id"] - a["id"];
+            })]
+        }
+        return merged_array;
     }
 
     return (
@@ -111,22 +163,22 @@ const Inbox = () => {
                     <div className="col-lg-4 blog-main">
 
                         <div className="blog-post Poruke">
-                            <p>Broj novih poruka: {messages.length}</p>
+                            <p>Broj novih poruka: {messagesRecieved.length}</p>
                             <hr className="round" />
-                            <button onClick={()=>{deleteAllRecieved(currentUser.id)}}>Delete All</button>
+                            <button onClick={() => { deleteAllRecieved(currentUser.id) }}>Delete All</button>
                             {
-                                messages.map(message => {
+                                messagesRecieved.map(message => {
                                     return (
                                         <div className={message.opened ? 'message opened' : 'message'} key={message.id}>
                                             <p>From: {findUsername(message.sender_id)}
-                                                <br />Date: {message.date.substr(8,2)+'/'+message.date.substr(5,2)+'/'+message.date.substr(0,4)} at {message.time}</p>
+                                                <br />Date: {message.date.substr(8, 2) + '/' + message.date.substr(5, 2) + '/' + message.date.substr(0, 4)} at {message.time}</p>
                                             <div className="message-text">
                                                 <p>
                                                     {message.text}
                                                 </p>
                                             </div>
-                                            <button onClick={()=>{replyFocus(findUsername(message.sender_id))}}>Reply</button>
-                                            <button onClick={()=>{deleteMessage(message.id)}}>Delete</button>
+                                            <button onClick={() => { replyFocus(findUsername(message.sender_id)) }}>Reply</button>
+                                            <button onClick={() => { deleteMessage(message.id) }}>Delete</button>
                                         </div>
                                     );
                                 })
@@ -139,19 +191,19 @@ const Inbox = () => {
                         <div className="blog-post Poruke">
                             <p>Poslano: {messagesSent.length}</p>
                             <hr className="round" />
-                            <button onClick={()=>{deleteAllSent(currentUser.id)}}>Delete All</button>
+                            <button onClick={() => { deleteAllSent(currentUser.id) }}>Delete All</button>
                             {
                                 messagesSent.map(message => {
                                     return (
                                         <div className='message' key={message.id}>
                                             <p>To: {findUsername(message.reciever_id)}
-                                                <br />Date: {message.date.substr(8,2)+'/'+message.date.substr(5,2)+'/'+message.date.substr(0,4)} at {message.time}</p>
+                                                <br />Date: {message.date.substr(8, 2) + '/' + message.date.substr(5, 2) + '/' + message.date.substr(0, 4)} at {message.time}</p>
                                             <div className="message-text">
                                                 <p>
                                                     {message.text}
                                                 </p>
                                             </div>
-                                            <button onClick={()=>{deleteMessage(message.id)}}>Delete</button>
+                                            <button onClick={() => { deleteMessage(message.id) }}>Delete</button>
                                         </div>
                                     );
                                 })
@@ -164,7 +216,7 @@ const Inbox = () => {
                         <div className="blog-post NovaPoruka">
                             <p>Nova poruka</p>
                             <hr className="round" />
-                            <Form acceptCharset="UTF-8" onSubmit={(e) => { e.preventDefault(); if(usernameError === ''){ e.target.reset(); setSentText('Message sent'); } }}>
+                            <Form acceptCharset="UTF-8" onSubmit={(e) => { e.preventDefault(); if (usernameError === '') { e.target.reset(); setSentText('Message sent'); } }}>
                                 <Form.Group controlId="newMessageUsername">
                                     <Form.Label srOnly>Prima:</Form.Label>
                                     <InputGroup className="mb-2">
@@ -184,6 +236,31 @@ const Inbox = () => {
                                     <Form.Text className="greenText">{sentText}</Form.Text>
                                 </Form.Group>
                             </Form>
+                        </div>
+
+                    </div>
+
+                </div>
+                <div className="row justify-content-center">
+
+                    <div className="col-lg-8 blog-main">
+
+                        <div className="blog-post Poruke">
+                            <p>Razgovori: {messages.length}</p>
+                            <hr className="round" />
+                            {
+                                messages.map(message => {
+                                    return (
+                                        <div className='message' key={message[0].id}>
+                                            <div className="message-text">
+                                                <p>
+                                                    {message[0].sender_id !== currentUser.id ? findUsername(message[0].sender_id) : findUsername(message[0].reciever_id)}
+                                                </p>
+                                            </div>
+                                        </div>
+                                    );
+                                })
+                            }
                         </div>
 
                     </div>
