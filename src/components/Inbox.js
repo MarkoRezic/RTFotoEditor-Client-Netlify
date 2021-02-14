188fa32b80ce70cba546521e96a46bdab450a790
Marko Rezić <markorezic1999@gmail.com>
2021-02-14 14:35:06 +0100

    optimizing chat

--- src/components/Inbox.js
+++ src/components/Inbox.js
@@ -117,22 +117,11 @@ const Inbox = () => {
             }
         }
         if (validUsername === 1 && text !== '') {
-            if (chat.other_id !== null) {
-                Axios.put(url + '/open-messages', { data: { sender_id: chat.other_id, reciever_id: currentUser.id } }).then((response) => {
-                    Axios.post(url + '/send-message', { sender_id: currentUser.id, reciever_id: findID(username), text: text }).then((response) => {
-                        console.log(response);
-                        updateMessages();
-                        setUsernameError('');
-                    });
-                });
-            }
-            else {
-                Axios.post(url + '/send-message', { sender_id: currentUser.id, reciever_id: findID(username), text: text }).then((response) => {
-                    console.log(response);
-                    updateMessages();
-                    setUsernameError('');
-                });
-            }
+            Axios.post(url + '/send-message', { sender_id: currentUser.id, reciever_id: findID(username), text: text }).then((response) => {
+                console.log(response);
+                updateMessages();
+                setUsernameError('');
+            });
         }
         else {
             updateMessages();
@@ -345,7 +334,8 @@ const Inbox = () => {
                                         })
                                     }
                                 </div>
-                                <Form onSubmit={(e) => { sendMessage(); e.preventDefault(); e.target.reset(); setText(''); }}>
+                                <Form onSubmit={(e) => { 
+            if (chat.other_id !== null) { Axios.put(url + '/open-messages', { data: { sender_id: chat.other_id, reciever_id: currentUser.id } }); }sendMessage(); e.preventDefault(); e.target.reset(); setText(''); }}>
                                     <Form.Group controlId="sendMessageInputID" className="sendMessageGroup">
                                         <Form.Label srOnly>Message</Form.Label>
                                         <InputGroup className="mb-2">
